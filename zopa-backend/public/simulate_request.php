264a0b0d6e8d05c5f5f7b91384fa4a1cea531419
DINESH KUMAR R <rdineshkumar88@gmail.com>
2026-08-06 13:20:54 +0530

    debug: fix simulate_request.php kernel class

--- zopa-backend/public/simulate_request.php
+++ zopa-backend/public/simulate_request.php
@@ -3,7 +3,7 @@ header('Content-Type: text/plain');
 
 require __DIR__ . '/../vendor/autoload.php';
 $app = require_once __DIR__ . '/../bootstrap/app.php';
-$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 $kernel->bootstrap();
 
 use App\Models\User;
